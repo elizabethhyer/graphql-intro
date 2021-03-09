24aa08164199c6aff44be1672b6eab6b78b64592
elizabethhyer <hyerhandmade@gmail.com>
2021-03-08 21:21:16 -0500

    Styled application

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,14 +11,11 @@ const app = express();
 app.use(cors());
 
 mongoose
-  .connect(
-    "mongodb+srv://liz:test123@gql-lesson.vhkox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      newUserUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: "myFirstDatabase",
-    }
-  )
+  .connect(process.env.CONNECT, {
+    newUserUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "myFirstDatabase",
+  })
   .then(() => console.log("connected to mongodb atlas"))
   .catch((err) => console.error(err.message));
 
